6f6a2d5c1bddcfe1adb9ba8e1cb2d86c545ef2d7
candytangsys <candytang1009@gmail.com>
2025-10-07 08:35:53 +0800

    Update Dialog1.java

--- Dialog1.java
+++ Dialog1.java
@@ -1,7 +1,10 @@
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;  
+//bialog box 對話框
+//JOptionPane 建立對話框
 
 public class Dialog1{
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"welcom to Java");
+        JOptionPane.showMessageDialog(null,"welcom to Java"); //message dialog 訊息對話框
     }
+
 }
